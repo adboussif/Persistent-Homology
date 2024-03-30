7ed15ff52d0cf444d74f05e61cb70460219a2976
adam  boussif <aboussif@pedago-ngs.univ-lyon1.fr>
2024-03-30 18:26:17 +0100

    bcl2 stat

--- scripts/smirnov.py
+++ scripts/smirnov.py
@@ -3,11 +3,11 @@ import pandas as pd
 from scipy.stats import ks_2samp
 
 # Charger les distances Ras-All
-ras_all_df = pd.read_csv('/data/home/aboussif/Projet14/deliverable/topologie/data/Ras/all/all_Ras.csv')
+ras_all_df = pd.read_csv('/data/home/aboussif/Projet14/deliverable/topologie/data/BCL2/all/all_bcl2.csv')
 distances_ras_all = ras_all_df['Distance'].values
 
 # Charger les distances Ras-Ras
-ras_ras_df = pd.read_csv('/data/home/aboussif/Projet14/deliverable/topologie/data/Ras/Ras/Ras_Ras.csv')
+ras_ras_df = pd.read_csv('/data/home/aboussif/Projet14/deliverable/topologie/data/BCL2/bcl2/silico/distance_results_barcode1.csv')
 distances_ras_ras = ras_ras_df['Distance'].values
 
 # Exécution du test de Kolmogorov-Smirnov
