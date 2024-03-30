24eb54843875361e028afb73bdd88e01fb95e7bb
adam boussif <adam.boussif@etu.univ-lyon1.fr>
2024-03-30 16:20:20 +0000

    Update fonction.py

--- scripts/fonction.py
+++ scripts/fonction.py
@@ -5,41 +5,36 @@ import pandas as pd
 import re
 
 def clean_name(name):
-    # Supprimer les préfixes "ref_" ou "target_" et les suffixes "_barcodeX.csv"
-    cleaned_name = re.sub(r'^(ref_|target_)?(\d\w{3})_ca_barcode\d\.csv$', r'\2', name)
-    return cleaned_name
-
+    # Retirer les préfixes 'target_' ou 'ref_' et les suffixes '_ca'
+    return re.sub(r'^(target_|ref_)?(.*?)(_ca)?$', r'\2', name)
 
 def load_and_merge_data(distances_path, ref_annotations_path, target_annotations_path):
     # Charger les données de distances
     distances_df = pd.read_csv(distances_path)
     
-    # Charger les données d'annotation cible
-    target_annotations_df = pd.read_csv(target_annotations_path)
-    
-    # Nettoyer les noms pour correspondre
+    # Nettoyer les colonnes pour correspondre aux noms
+    distances_df['Reference'] = distances_df['Reference'].apply(clean_name)
     distances_df['Target'] = distances_df['Target'].apply(clean_name)
     
-    # Nettoyer les noms de référence même sans fichier d'annotations spécifique
-    distances_df['Reference'] = distances_df['Reference'].apply(clean_name)
+    # Charger les données d'annotation cible et nettoyer
+    target_annotations_df = pd.read_csv(target_annotations_path)
+    target_annotations_df['PDB_ID'] = target_annotations_df['PDB_ID'].apply(clean_name)
     
     # Fusionner les données de target
-    merged_df = distances_df.merge(target_annotations_df.rename(columns={'Function':'Function Target'}),
-                                   left_on='Target', right_on='PDB_ID', how='left')
+    merged_df = distances_df.merge(target_annotations_df, left_on='Target', right_on='PDB_ID', how='left')
     
-    # Inclure la colonne 'Distance' dans le DataFrame final
-    final_columns = ['Target', 'Function Target', 'Number of Alpha Carbons in Target', 'Distance', 'Reference']
     if ref_annotations_path:
-        # Charger les données d'annotation de référence si spécifié
+        # Charger les données d'annotation de référence si spécifié et nettoyer
         ref_annotations_df = pd.read_csv(ref_annotations_path)
-        merged_df = merged_df.merge(ref_annotations_df.rename(columns={'Function':'Function Reference'}),
-                                    left_on='Reference', right_on='PDB_ID', how='left')
-        final_columns += ['Function Reference', 'Number of Alpha Carbons in Reference']
-    else:
-        # Sinon, ajouter des colonnes vides pour la référence
-        merged_df['Function Reference'] = ''
-        merged_df['Number of Alpha Carbons in Reference'] = ''
-
+        ref_annotations_df['PDB_ID'] = ref_annotations_df['PDB_ID'].apply(clean_name)
+        # Fusionner les données de référence
+        merged_df = merged_df.merge(ref_annotations_df, left_on='Reference', right_on='PDB_ID', how='left', suffixes=('_Target', '_Reference'))
+    
+    # Sélectionner et renommer les colonnes pour le DataFrame final
+    final_columns = ['Target', 'Function_Target', 'Number of Alpha Carbons in Target', 'Distance']
+    if ref_annotations_path:
+        final_columns += ['Reference', 'Function_Reference', 'Number of Alpha Carbons in Reference']
+    
     final_df = merged_df[final_columns]
     
     return final_df
@@ -57,7 +52,7 @@ def main():
     
     args = parser.parse_args()
     
-    final_df = load_and_merge_data(args.distance, args.reference, args.target)
+    final_df = load_and_merge_data(args.distance, args.reference if args.reference else None, args.target)
     save_to_csv(final_df, args.output)
 
 if __name__ == "__main__":
