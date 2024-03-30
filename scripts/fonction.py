e169f4b3082864172dea463e573db6825aa90af5
adam boussif <adam.boussif@etu.univ-lyon1.fr>
2024-03-30 16:33:40 +0000

    Update fonction.py

--- scripts/fonction.py
+++ scripts/fonction.py
@@ -5,15 +5,16 @@ import pandas as pd
 import re
 
 def clean_name(name):
-    # Retirer les préfixes 'target_' ou 'ref_' et les suffixes '_ca'
-    return re.sub(r'^(target_|ref_)?(.*?)(_ca)?$', r'\2', name)
+    # Retirer les préfixes 'target_' ou 'ref_' et les suffixes '_ca' ou '_barcode'
+    return re.sub(r'^(target_|ref_)?(.*?)(_ca|_barcode)?$', r'\2', name)
 
 def load_and_merge_data(distances_path, ref_annotations_path, target_annotations_path):
     # Charger les données de distances
     distances_df = pd.read_csv(distances_path)
     
-    # Nettoyer les colonnes pour correspondre aux noms
+    # Nettoyer les noms dans les colonnes 'Target' et 'Reference'
     distances_df['Target'] = distances_df['Target'].apply(clean_name)
+    distances_df['Reference'] = distances_df['Reference'].apply(clean_name)
     
     # Charger les données d'annotation cible et nettoyer
     target_annotations_df = pd.read_csv(target_annotations_path)
@@ -23,22 +24,19 @@ def load_and_merge_data(distances_path, ref_annotations_path, target_annotations
     merged_df = distances_df.merge(target_annotations_df[['PDB_ID', 'Function']],
                                    left_on='Target', right_on='PDB_ID', how='left')
     merged_df.rename(columns={'Function': 'Function Target'}, inplace=True)
-    
-    # Si ref_annotations_path est fourni, charger les données de référence et fusionner
+
+    # Charger et fusionner les données de référence si fournies
     if ref_annotations_path:
         ref_annotations_df = pd.read_csv(ref_annotations_path)
         ref_annotations_df['PDB_ID'] = ref_annotations_df['PDB_ID'].apply(clean_name)
         merged_df = merged_df.merge(ref_annotations_df[['PDB_ID', 'Function']],
-                                    left_on='Reference', right_on='PDB_ID', how='left', suffixes=('', '_Reference'))
+                                    left_on='Reference', right_on='PDB_ID', how='left')
         merged_df.rename(columns={'Function': 'Function Reference'}, inplace=True)
 
     # Inclure la colonne 'Distance' dans le DataFrame final
-    final_columns = ['Target', 'Function Target', 'Number of Alpha Carbons in Target', 'Distance']
-    if ref_annotations_path:
-        final_columns += ['Reference', 'Function Reference', 'Number of Alpha Carbons in Reference']
-    
-    final_df = merged_df[final_columns]
-    
+    final_df = merged_df[['Target', 'Function Target', 'Number of Alpha Carbons in Target', 
+                          'Distance', 'Reference', 'Function Reference', 'Number of Alpha Carbons in Reference']]
+
     return final_df
 
 def save_to_csv(df, output_path):
@@ -54,7 +52,7 @@ def main():
     
     args = parser.parse_args()
     
-    final_df = load_and_merge_data(args.distance, args.reference if args.reference else None, args.target)
+    final_df = load_and_merge_data(args.distance, args.reference, args.target)
     save_to_csv(final_df, args.output)
 
 if __name__ == "__main__":
